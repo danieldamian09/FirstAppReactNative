ae1d55fd99e3d4a5220fadb01432aed9eb001e14
danieldamian09 <humbertorivero9@gmail.com>
2022-05-15 19:26:05 -0300

    Feat padding, margin, border, width and height

--- src/screens/BoxObjectModelScreen.tsx
+++ src/screens/BoxObjectModelScreen.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const BoxObjectModelScreen = () => {
   return (
-    <View>
-      <Text>Box Object Model</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>Box Object Model</Text>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'red',
+  },
+  title: {
+    fontSize: 20,
+    paddingHorizontal: 100,
+    paddingVertical: 50,
+    marginHorizontal: 50,
+    // marginRight: 10,
+    // marginLeft: 10,
+    // padding: 20,
+    // width: 250,
+    borderWidth: 10,
+    // backgroundColor: 'red',
+  },
+});
 
 export default BoxObjectModelScreen;
